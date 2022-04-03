d8dd4d6201eef0322ee10b6d4366787d59e410f3
Ricardo Viteri <rviteri@estratega.ec>
2022-04-03 12:37:02 -0500

    Remove most blurs

--- README.md
+++ README.md
@@ -1,4 +1,9 @@
 # El-Universo-Unblocker
+
 Script to unblock blurring at www.eluniverso.com
 
-The script is unptimized, but good enough. There is an infite loop running every one second to remove the CSS class that blurs the text
+The script is unptimized, but good enough.
+
+# How to use
+
+Go to the page where content is blurred, go to dev tools, copy and paste the code from index.js
---
+++ index.js
@@ -0,0 +1,38 @@
+const sleep = (ms) =>
+  new Promise((resolve) => {
+    setTimeout(() => {
+      resolve();
+    }, ms);
+  });
+
+(async () => {
+  // Delete template message
+
+  // Delete subscription message
+  const subscription = document.getElementById("modal_planes_register");
+  subscription.remove();
+
+  // Unblur everytime content gets blurred
+  while (true) {
+    var allClasses = [];
+
+    var allElements = document.querySelectorAll("*");
+
+    for (var i = 0; i < allElements.length; i++) {
+      var classes = allElements[i].className.toString().split(/\s+/);
+      for (var j = 0; j < classes.length; j++) {
+        var cls = classes[j];
+        if (cls && allClasses.indexOf(cls) === -1) allClasses.push(cls);
+      }
+    }
+
+    allClasses.forEach((el) => {
+      var els = document.getElementsByClassName(el);
+      for (var i = 0; i < els.length; i++) {
+        els[i].style.removeProperty("filter");
+      }
+    });
+
+    await sleep(750);
+  }
+})();
